177ad3c6486dc4a602d49f3107dfd5ab6177c4f3
GaiaTuven <ggwiese@gmail.com>
2026-03-11 07:25:08 +0200

    User input and function declarations

--- Week_2_Arrays/problem_sets/readability/readability.c
+++ Week_2_Arrays/problem_sets/readability/readability.c
@@ -3,13 +3,42 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(void) {
+
+  // Variables to store count of letters, words, sentences
+  char userInput[256];
+  char sentence[256];
+  char letters[256];
+  char words[256];
+
+  // Prompt the user for some text
+  printf("Enter some text: \n");
+  fgets(userInput, sizeof(userInput), stdin);
+  printf("%s", userInput);
+
+  // Count the number of letters, words, and sentences in the text
+
+  // Compute the Coleman-Liau index
+
+  // Print the grade level
+}
+
+
+// Count letters in the text
+int total_amount_of_letters(char s)
 {
-    // Prompt the user for some text
+  // code goes here 
+}
 
-    // Count the number of letters, words, and sentences in the text
 
-    // Compute the Coleman-Liau index
+// Count words in text
+int total_amount_of_words(char s)
+{
+  // code goes here 
+}
 
-    // Print the grade level
+// Count sentences in the text
+int total_amount_of_sentences(char s)
+{
+  // code goes here 
 }
