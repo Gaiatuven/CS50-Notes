08d5347ebf6d7ed2ab7755aa7fbcbdd1c7efd066
GaiaTuven <ggwiese@gmail.com>
2026-03-12 06:47:23 +0200

    Function to calculate count of letters

--- Week_2_Arrays/problem_sets/readability/readability.c
+++ Week_2_Arrays/problem_sets/readability/readability.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int total_amount_of_letters(char *userInput);
+
 int main(void) {
 
   // Variables to store count of letters, words, sentences
@@ -11,12 +13,18 @@ int main(void) {
   char letters[256];
   char words[256];
 
+  int result;
+
+
   // Prompt the user for some text
   printf("Enter some text: \n");
   fgets(userInput, sizeof(userInput), stdin);
   printf("%s", userInput);
 
   // Count the number of letters, words, and sentences in the text
+
+  result = total_amount_of_letters(userInput);
+  printf("%d\n", result);
 
   // Compute the Coleman-Liau index
 
@@ -25,20 +33,30 @@ int main(void) {
 
 
 // Count letters in the text
-int total_amount_of_letters(char s)
+int total_amount_of_letters(char *userInput)
 {
-  // code goes here 
+    int amount_of_letters = 0;
+
+    for (int i = 0; userInput[i] != '\0'; i++)
+    {
+        if (isalpha((unsigned char)userInput[i]))
+        {
+            amount_of_letters++;
+        }
+    }
+
+    return amount_of_letters;
 }
 
 
-// Count words in text
-int total_amount_of_words(char s)
-{
-  // code goes here 
-}
+// // Count words in text
+// int total_amount_of_words(char s)
+// {
+//   // code goes here 
+// }
 
-// Count sentences in the text
-int total_amount_of_sentences(char s)
-{
-  // code goes here 
-}
+// // Count sentences in the text
+// int total_amount_of_sentences(char s)
+// {
+//   // code goes here 
+// }
