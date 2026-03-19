fd71a2859bfb6e85bbcc02661bb6e2ea14e72a35
GaiaTuven <ggwiese@gmail.com>
2026-03-19 18:40:18 +0200

    Function to shift letters

--- Week_2_Arrays/problem_sets/caesar/ceasar.c
+++ Week_2_Arrays/problem_sets/caesar/ceasar.c
@@ -1,9 +1,14 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 bool only_digits(char *str);
+void rotate(int k, char *str);
+char *user_text(void);
+
+char *text;
 
 int main(int argc, char *argv[]) {
   // Make sure program was run with just one command-line argument
@@ -18,15 +23,17 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Make sure every character in argv[1] is a digit
-
   // Convert argv[1] from a `string` to an `int`
 
-  // Prompt user for plaintext
+  int k = atoi(argv[1]);
+  char *plaintext = user_text();
 
-  // For each character in the plaintext:
+  printf("ciphertext: ");
+  rotate(k, plaintext);
+  printf("\n");
 
-  // Rotate the character if it's a letter
+  free(plaintext);
+  return 0;
 }
 
 bool only_digits(char *str) {
@@ -36,4 +43,30 @@ bool only_digits(char *str) {
     }
   }
   return true;
+}
+
+// Prompt user for plaintext
+char *user_text(void) {
+  char *plaintext = malloc(1024);
+  if (plaintext == NULL)
+    return NULL;
+
+  printf("Plaintext: ");
+  fgets(plaintext, 1024, stdin);
+
+  plaintext[strcspn(plaintext, "\n")] = '\0';
+  return plaintext;
+}
+
+void rotate(int k, char *str) {
+  for (int i = 0; i < strlen(str); i++) {
+    if (isupper(str[i])) {
+      // (Current index + key) % 26 + ASCII offset for 'A'
+      printf("%c", (str[i] - 'A' + k) % 26 + 'A');
+    } else if (islower(str[i])) {
+      printf("%c", (str[i] - 'a' + k) % 26 + 'a');
+    } else {
+      printf("%c", str[i]);
+    }
+  }
 }
