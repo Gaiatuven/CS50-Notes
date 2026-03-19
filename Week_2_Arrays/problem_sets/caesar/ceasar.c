23298f20563dfce84dced1410aed8bb6147768de
GaiaTuven <ggwiese@gmail.com>
2026-03-19 13:38:03 +0200

    If statement chekcing if argc is equal to 2 and is digit

--- Week_2_Arrays/problem_sets/caesar/ceasar.c
+++ Week_2_Arrays/problem_sets/caesar/ceasar.c
@@ -1,17 +1,39 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, string argv[])
-{
-    // Make sure program was run with just one command-line argument
+bool only_digits(char *str);
 
-    // Make sure every character in argv[1] is a digit
+int main(int argc, char *argv[]) {
+  // Make sure program was run with just one command-line argument
+  if (argc != 2) {
+    printf("Usage: ./caesar key\n");
+    return 1;
+  }
 
-    // Convert argv[1] from a `string` to an `int`
+  // Make sure every character in argv[1] is a digit
+  if (!only_digits(argv[1])) {
+    printf("Usage: ./caesar key\n");
+    return 1;
+  }
 
-    // Prompt user for plaintext
+  // Make sure every character in argv[1] is a digit
 
-    // For each character in the plaintext:
+  // Convert argv[1] from a `string` to an `int`
 
-        // Rotate the character if it's a letter
+  // Prompt user for plaintext
+
+  // For each character in the plaintext:
+
+  // Rotate the character if it's a letter
+}
+
+bool only_digits(char *str) {
+  for (int i = 0; i < strlen(str); i++) {
+    if (!isdigit(str[i])) {
+      return false;
+    }
+  }
+  return true;
 }
