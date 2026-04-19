8f4e898ab7f1f7f7e93f810cac5b7e659be18c98
GaiaTuven <ggwiese@gmail.com>
2026-04-19 11:47:00 +0200

    Function - find min votes

--- Week_3_Algorithms/problem_sets/runoff/main.c
+++ Week_3_Algorithms/problem_sets/runoff/main.c
@@ -172,9 +172,17 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min = voter_count;
+    for (int i = 0; i < candidates[i].votes; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
 }
+
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
