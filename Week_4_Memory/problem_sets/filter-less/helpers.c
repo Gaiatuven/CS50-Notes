7c57da03e951a2bb936afa4bc18f4511572f857d
GaiaTuven <ggwiese@gmail.com>
2026-05-14 07:07:44 +0200

    Started nested for loop for firt function

--- Week_4_Memory/problem_sets/filter-less/helpers.c
+++ Week_4_Memory/problem_sets/filter-less/helpers.c
@@ -3,6 +3,20 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            uint8_t red = image[i][j].rgbtRed;
+            uint8_t green = image[i][j].rgbtGreen;
+            uint8_t blue = image[i][j].rgbtBlue;
+
+            // Take average of red, green, and blue
+        }
+    }
+
+    // Update pixel values
     return;
 }
 
